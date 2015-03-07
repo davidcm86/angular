c13a695cde456baf714cb1b371d23ef30cd7835a
david <dcarretero86@hotmail.com>
2015-03-07 22:30:57 +0100

    Routes y la carga de view sin cambiar de view

---
+++ js/10-routes.js
@@ -0,0 +1,23 @@
+angular
+	.module('routesApp', ['ngRoute']) // directiva para cargar rutas
+	.config(function($routeProvider){ // routeProvider contiene los metodos
+		$routeProvider
+			.when("/", {
+				controller: "rutaController",
+				controllerAs: "rc",
+				templateUrl: "10-routes.html"
+			})
+			.when("/10-bienvenida", {
+				controller: "rutaController",
+				controllerAs: "rc",
+				templateUrl: "10-bienvenida.html"
+			})
+			.when("/10-despedida", {
+				controller: "rutaController",
+				controllerAs: "rc",
+				templateUrl: "10-despedida.html"
+		});
+	})
+	.controller('rutaController', function(){
+		console.log('El controlador rutaController');
+	});
