5832f95b32ecfa3d66d1bfe9010130c3fe00f58d
david <dcarretero86@hotmail.com>
2015-03-07 10:54:02 +0100

    App Rest de Cervezas

---
+++ js/8-api-rest.js
@@ -0,0 +1,17 @@
+angular
+	.module('apiApp', [])
+	.controller('apiAppCtrl', controladorPrincipal);
+
+	function controladorPrincipal($scope, $http){
+		var vm = this;
+		vm.buscaCervezas = function(){
+			var url = "http://api.openbeerdatabase.com/v1/beers.json?callback=JSON_CALLBACK";
+			if(vm.nombre){
+				url += "&query=" + vm.nombre;
+			}
+			$http.jsonp(url).success(function(respuesta){
+				console.log("res:", respuesta);
+				vm.cervezas = respuesta.beers;
+			});
+		}
+}
